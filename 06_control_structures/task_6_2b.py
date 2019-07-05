1fe0da8541fa63bc615c2772ebd2ce11960abb07
inickp <i.nickp@gmail.com>
2019-07-05 15:59:55 -0400

    task6.2b completed

--- 06_control_structures/task_6_2b.py
+++ 06_control_structures/task_6_2b.py
@@ -9,3 +9,25 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+
+valid = 0
+while valid != 1:
+  ip_input = raw_input ('IP: ')
+  for oct in ip_input.split('.'):
+    if (len(ip_input.split('.')) != 4) or not (oct.isdigit()) or (int(oct) < 0) or (int(oct) > 255):
+      print('wrong IP')
+      break
+    else:
+      valid = 1 
+
+if ip_input == '255.255.255.255':
+  print('local broadcast')
+elif ip_input == '0.0.0.0':
+  print ('unassigned')
+elif int(ip_input.split('.')[0])  <= 223 and int(ip_input.split('.')[0]) >= 1:
+  print ('unicast')
+elif int(ip_input.split('.')[0]) >= 224 and  int(ip_input.split('.')[0]) <=239:
+  print ('multicast')
+else:
+  print ('unused')
+
