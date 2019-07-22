99b51d4b5fa2e62019e8ee0a762397e8bb871845
inickp <i.nickp@gmail.com>
2019-07-22 04:13:36 -0400

    task_9.4 completed

--- 09_functions/task_9_4.py
+++ 09_functions/task_9_4.py
@@ -20,7 +20,8 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
 
-ignore = ['duplex', 'alias', 'Current configuration']
+ignore = ['duplex', 'alias', 'Current configuration', '!']
+
 
 
 def ignore_command(command, ignore):
@@ -31,7 +32,41 @@ def ignore_command(command, ignore):
     ignore - список. Список слов
 
     Возвращает
-    * True, если в команде содержится слово из списка ignore
+    * True, если в ignore_commandкоманде содержится слово из списка ignore
     * False - если нет
     '''
+
+
     return any(word in command for word in ignore)
+
+
+
+
+
+def list_glob_commands(commands_list):
+  glob_commands_list = []
+  for str in commands_list:
+    if not str.startswith(' ') and not ignore_command(str, ignore) and not len(str)==0:
+      glob_commands_list.append(str)
+  return glob_commands_list
+
+
+def list_subcommands(glob_command, commands_list):
+  i = commands_list.index(glob_command) + 1
+  subcommands_list = []
+  while commands_list[i].startswith(' '):
+    subcommands_list.append(commands_list[i])
+    i = i+1
+  return subcommands_list
+
+
+
+
+file = open('config_sw1.txt')
+list = file.read().split('\n')
+glob_commands = list_glob_commands(list)
+config_dict = {key: list_subcommands(key, list) for key in glob_commands}
+print(config_dict)
+
+
+
